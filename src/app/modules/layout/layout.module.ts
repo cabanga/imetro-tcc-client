0f0c94f188c7548e3190862fc211a894a3d8aa55
Bruno Vivaldo <brunolumeca@live.com>
2021-09-16 16:25:34 +0100

    login component added

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component'; 
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     BrowserAnimationsModule
   ],
   exports : [
-    MenuComponent
+    MenuComponent,
+    LoginComponent
   ]
 })
 export class LayoutModule { }
