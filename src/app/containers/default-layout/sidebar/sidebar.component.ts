33f41076aca2e94ba3effa6c16ad33a0f145c293
cabanga <manucabanga@gmail.com>
2022-02-20 13:38:49 +0100

    this commit Config dashboard redirect

--- src/app/containers/default-layout/sidebar/sidebar.component.ts
+++ src/app/containers/default-layout/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/views/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  logOut(){
+    this._authService.removeTokenOfUser()
+}
 
 }
